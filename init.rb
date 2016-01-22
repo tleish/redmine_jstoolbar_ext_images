d494a2365f59735c5a12a93e26d25b44a04f2882
Thomas Leishman <thomas.leishman@plansource.com>
2016-01-22 15:57:56 -0700

    Adding default attributes to thumbnail markup

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_jstoolbar_ext_images do
   name 'Redmine jsToolbar Images Extension'
   author 'Thomas Leishman'
   description 'The Redmine JS Toolbar Images Extension add additional buttons to the jsToolbar.'
-  version '0.3.0'
+  version '0.3.1'
   url 'https://github.com/tleish'
   author_url 'https://github.com/tleish'
   requires_redmine_plugin :redmine_jstoolbar_ext, :version_or_higher => '0.1.0'
